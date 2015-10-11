5a0a6a0aeba5d0424648150d74dfab688b82b77d
Tom Akehurst <t.m.akehurst@googlemail.com>
2015-10-11 17:03:34 +0100

    Add fault injection example

---
+++ src/test/java/Six.java
@@ -0,0 +1,40 @@
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import javax.ws.rs.ProcessingException;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.http.Fault.EMPTY_RESPONSE;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+public class Six {
+
+    @Rule
+    public WireMockRule wm = new WireMockRule();
+
+    NewsService newsService;
+
+    @Before
+    public void init() {
+        newsService = new NewsService();
+    }
+
+    @Test
+    public void premature_socket_closure_fault() throws Exception {
+        wm.stubFor(get(urlPathEqualTo("/search"))
+                .willReturn(aResponse().withFault(EMPTY_RESPONSE)));
+
+        try {
+            newsService.getFirstSoftwareHeadline();
+            fail();
+        } catch (Exception e) {
+            assertThat(e, instanceOf(ProcessingException.class));
+            assertThat(e.getMessage(), containsString("Unexpected end of file"));
+        }
+    }
+}
